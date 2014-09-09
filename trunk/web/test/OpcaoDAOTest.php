68865258e64a00937e230b3588f2a45fb4f34d61
ivanknow <ivanknow@ebf532ed-8084-768a-e56a-26ba09cdccf5>
2014-09-09 21:29:16 +0000

    Questões

---
+++ trunk/web/test/OpcaoDAOTest.php
@@ -0,0 +1,40 @@
+<?php
+class TurmaDAOTest extends PHPUnit_Framework_TestCase {
+
+	public function testInsert() {
+		
+		$questaoFechada = new QuestaoFechada(1);
+		
+		$opcao = new Opcao(1,"Sim",$questaoFechada);
+		
+		$dao = new OpcaoDAO();
+		
+		$id = $dao->inserir($opcao);
+		
+		$opcaoTemp = new Opcao($id);
+		
+		$result = $dao->buscarTodos($opcaoTemp);
+		
+		$this->assertEquals (1,count($result));
+		
+	}
+
+
+	public function testDelete() {
+
+		$dao = new OpcaoDAO();
+		
+		$opcaoTemp = new Opcao(1);
+		
+		$result = $dao->buscarTodos($opcaoTemp);
+		
+		$this->assertEquals (1,count($result));
+		
+		$dao->apagar($result[0]);
+		
+		$result = $dao->buscarTodos($opcaoTemp);
+		
+		$this->assertEquals (0,count($result));
+		
+	}
+}
