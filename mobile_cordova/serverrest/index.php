3b0acde75343ba04bbc805bedaaa67e3d74bb1b2
ivanknow <ivanknow@ebf532ed-8084-768a-e56a-26ba09cdccf5>
2015-01-19 17:37:56 +0000

    javascript e php

--- mobile_cordova/serverrest/index.php
+++ mobile_cordova/serverrest/index.php
@@ -6,41 +6,51 @@ include 'Slim/Slim.php';
 $app = new \Slim\Slim ();
 $app->post ( '/prova/', 'getAllProva' );
 $app->post ( '/prova/:id', 'getProvaById' );
+$app->get ( '/prova/:id', 'getProvaById' );
 
 $app->run ();
 function getAllProva() {
-	$retorno =array("items"=> array (
-			array (
-					"titulo" => "Prova 1",
-					"id" => "1",
-					
-			),array (
-					"titulo" => "Prova 2",
-					"id" => "2",
-					
-			),array (
-					"titulo" => "Prova 3",
-					"id" => "3",
-					
+	$retorno = array (
+			"items" => array (
+					array (
+							"titulo" => "Prova 1",
+							"id" => "1" 
+					),
+					array (
+							"titulo" => "Prova 2",
+							"id" => "2" 
+					),
+					array (
+							"titulo" => "Prova 3",
+							"id" => "3" 
+					) 
 			)
-	));
+			 
+	);
 	
 	echo json_encode ( $retorno );
 }
 function getProvaById($id) {
 	$retorno = array (
+			"item" => array (
 					"titulo" => "Prova 1",
 					"id" => "1",
 					"count" => "15",
+					"data" => "15/02/2015",
 					"autor" => "Gilberto",
-					"questoes" => array(
-					array("enunciado"=>"Qual é a cor?"),
-					array("enunciado"=>"Porque tem cor?"),
-					array("enunciado"=>"Onde a cor fica visivel?")
-					
-					)
-					
-			
+					"questoes" => array (
+							array (
+									"enunciado" => "Qual eh a cor?" 
+							),
+							array (
+									"enunciado" => "Porque tem cor?" 
+							),
+							array (
+									"enunciado" => "Onde a cor fica visivel?" 
+							) 
+					) 
+			)
+			 
 	);
 	
 	echo json_encode ( $retorno );
