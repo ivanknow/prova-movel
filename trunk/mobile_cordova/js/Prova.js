53092588f7a0e9334f36c214b8782ce2ac65508e
ivanknow <ivanknow@ebf532ed-8084-768a-e56a-26ba09cdccf5>
2015-01-20 15:39:34 +0000

    javascript

--- trunk/mobile_cordova/js/Prova.js
+++ trunk/mobile_cordova/js/Prova.js
@@ -38,30 +38,9 @@ ProvaController.setShowItem(function(item) {
 	$('#tableDadosProva  tr:last td:eq(1)').html(item.autor);
 	$('#tableDadosProva  tr:last td:eq(2)').html(item.data);
 	$('#tableDadosProva  tr:last td:eq(3)').html(item.questoes.length);	
-	ProvaController.item = item;
+	ProvaExecucaoController.provaSelecionada = item;
 });
 
 $(document).on("pagebeforecreate", "#home", function() {
 	ProvaController.init();
-});
-
-$(document).on("pagebeforecreate", "#responder", function() {
-	var questao = ProvaController.item.questoes[ProvaController.QuestaoSelecionadaIndex];
-	$('#questaoEnunciado').html(questao.enunciado);
-	
-	$(".divResposta").each(function() {
-		$(this).hide();
-	});
-	
-	switch(questao.tipo){
-	case 0: // aberto
-		//mostra aberta
-		$("#fieldRespostaAberta").show();
-	break;
-	
-	case 1: // fechada
-		//mostra fechada
-		$("#fieldRespostaFechada").show();
-	break;
-	}
 });
